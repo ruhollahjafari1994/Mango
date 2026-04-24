3b5452e3abac71aacf1f448c90061ede878f6641
Roohollah Jafari <roohollah.jafari@nexttoptech.org>
2026-04-25 00:41:51 +0330

    Add Authentication service

---
+++ Mango.Services.AuthAPI/Service/AuthService.cs
@@ -0,0 +1,100 @@
+﻿using Mango.Services.AuthAPI.Data;
+using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Models.Dto;
+using Mango.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Services.AuthAPI.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly AppDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+        public AuthService(UserManager<ApplicationUser> userManager, AppDbContext db, RoleManager<IdentityRole> roleManager, IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _userManager = userManager;
+            _db = db;
+            _roleManager = roleManager;
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+        public async Task<string> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            ApplicationUser user = new()
+            {
+                UserName = registrationRequestDto.Email,
+                Email = registrationRequestDto.Email,
+                NormalizedEmail = registrationRequestDto.Email.ToUpper(),
+                Name = registrationRequestDto.Name,
+                PhoneNumber = registrationRequestDto.PhoneNumber,
+            };
+            try
+            {
+                var result = await _userManager.CreateAsync(user, registrationRequestDto.Password);
+                if (result.Succeeded)
+                {
+                    var userToReturn = _db.ApplicationUsers.First(u => u.UserName == registrationRequestDto.Email);
+                    UserDto userDto = new()
+                    {
+                        Email = registrationRequestDto.Email,
+                        Name = registrationRequestDto.Name,
+                        PhoneNumber = registrationRequestDto.PhoneNumber,
+                        ID = userToReturn.Id
+                    };
+                    return "";
+
+                }
+                else
+                {
+                    return result.Errors.FirstOrDefault().Description;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            return "Error Encountered";
+        }
+        public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDto.UserName.ToLower());
+            bool isValid = await _userManager.CheckPasswordAsync(user, loginRequestDto.Password);
+
+            if (user is null || isValid == false)
+            {
+                return new LoginResponseDto() { User = null, Token = "" };
+            }
+            UserDto userDto = new()
+            {
+                Email = user.Email,
+                Name = user.Name,
+                ID = user.Id,
+                PhoneNumber = user.PhoneNumber
+            };
+            var token = _jwtTokenGenerator.GenerateToken(user);
+            LoginResponseDto loginResponseDto = new LoginResponseDto()
+            {
+                User = userDto,
+                Token = token
+            };
+            return loginResponseDto;
+        }
+
+        public async Task<bool> AssignRole(string email, string roleName)
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(u => u.Email.ToLower() == email.ToLower());
+            if (user is not null)
+            {
+                if (!_roleManager.RoleExistsAsync(roleName).GetAwaiter().GetResult())
+                {
+                    _roleManager.CreateAsync(new IdentityRole(roleName)).GetAwaiter().GetResult();
+                }
+                await _userManager.AddToRoleAsync(user, roleName);
+                return true;
+            }
+            return false;
+        }
+    }
+}
